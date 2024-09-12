4fb0f72ac33d72449ad9df485cd674f759f356a5
ola <oyakubovska80@gmail.com>
2024-09-12 21:44:03 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import styles from "./app.module.css";
 
-function App() {
+export const App = () => {
+  const [value, setValue] = useState("");
+  const [error, setError] = useState("");
+  const [list, setList] = useState([]);
+
+  const [isValueValid, setIsValueValid] = useState(false);
+
+  const onInputButtonClick = () => {
+    const promptValue = prompt("Введите значение:");
+
+    if (promptValue.length < 3) {
+      setError("Ошибка!");
+    }
+
+    setValue(promptValue);
+    console.log(promptValue);
+
+    setIsValueValid(true);
+  };
+
+  const onAddButtonClick = () => {
+    const updatedList = [...list, { id: Date.now(), value }];
+    setList(updatedList);
+    console.log(list);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.app}>
+      {error ? (
+        <div className={styles.error}>{error}</div>
+      ) : (
+        <>
+          <h1>ВВОД ЗНАЧЕНИЯ </h1>
+          <h3>
+            Текущее значение: <output className={styles.text}>{value}</output>
+          </h3>
+          <div className={styles.buttons}>
+            <button onClick={onInputButtonClick}> Ввести новое</button>
+            <button disabled={!isValueValid} onClick={onAddButtonClick}>
+              Добавить в список
+            </button>
+          </div>
+          <h2> Список</h2>
+          {list.length > 0 ? (
+            list.map((item) => (
+              <div className={styles.text} key={item.id}>
+                {" "}
+                {item.value}
+              </div>
+            ))
+          ) : (
+            <div className={styles.emptiness}>Нет добавленных элементов </div>
+          )}
+        </>
+      )}
     </div>
   );
-}
-
-export default App;
+};
